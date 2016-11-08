9e11e564ffcb23273b0ddfcb2085727a59e3196a
Yifan Yu <yvifan@gmail.com>
2016-11-09 01:21:04 +0200

    ./runclash.sh :l MAC.hs :verilog

---
+++ MAC.hs
@@ -0,0 +1,15 @@
+module MAC where
+
+import CLaSH.Prelude
+
+ma acc (x,y) = acc + x * y
+
+macT acc (x,y) = (acc',o)
+  where
+    acc' = ma acc (x,y)
+    o    = acc
+
+mac = mealy macT 0
+
+topEntity :: Signal (Signed 9, Signed 9) -> Signal (Signed 9)
+topEntity = mac
