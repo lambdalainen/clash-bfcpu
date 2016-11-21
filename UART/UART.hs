545346543c38b2734bca4d2f3c78c320217e3a53
Yifan Yu <yvifan@gmail.com>
2016-11-21 17:47:43 +0200

    Convert topEntity in UART.UART to a usable function

--- UART/UART.hs
+++ UART/UART.hs
@@ -1,19 +1,24 @@
-module UART.UART where
+module UART.UART (uart) where
 
 import CLaSH.Prelude
 import UART.Baud
 import UART.Rx
 
-{-# ANN topEntity
-  (defTop
-    { t_name     = "uart"
-    , t_inputs   = ["RsRx"]
-    , t_outputs  = ["led"]
-    , t_extraIn  = [("clk", 1)]
-    , t_extraOut = []
-    , t_clocks   = []
-    }) #-}
-topEntity :: Signal Bit -> Signal (BitVector 8)
-topEntity rx = _b_reg <$> rx_reg
+-- {-# ANN topEntity
+--   (defTop
+--     { t_name     = "uart"
+--     , t_inputs   = ["RsRx"]
+--     , t_outputs  = ["led"]
+--     , t_extraIn  = [("clk", 1)]
+--     , t_extraOut = []
+--     , t_clocks   = []
+--     }) #-}
+-- topEntity :: Signal Bit -> Signal (BitVector 8)
+-- topEntity rx = _b_reg <$> rx_reg
+--   where
+--   rx_reg  = register rxInit (rxRun <$> rx_reg <*> rx <*> baud_tick)
+
+uart :: Signal Bit -> (Signal Bit, Signal (BitVector 8))
+uart rx = (_rx_done_tick <$> rx_reg, _b_reg <$> rx_reg)
   where
   rx_reg  = register rxInit (rxRun <$> rx_reg <*> rx <*> baud_tick)
