ea083169b81417eb5f7206198b16da71861eafc6
Yifan Yu <yvifan@gmail.com>
2016-11-21 22:10:16 +0200

    Trivial

--- Tutorial.hs
+++ Tutorial.hs
@@ -1,7 +1,7 @@
 -- Create a constant Signal from a combinational value
 signal :: a -> Signal a
 
->>> sampleN 5 (signal 4 :: Signal Int)
+> sampleN 5 (signal 4 :: Signal Int)
 [4,4,4,4,4]
 
 -- generalisation
@@ -10,5 +10,5 @@ signal :: Applicative f => a -> f a
 -- initial value -> signal to delay for 1 cycle
 register :: a -> Signal a -> Signal a 
 
->>> sampleN 3 (register 8 (fromList [1,2,3,4]))
+> sampleN 3 (register 8 (fromList [1,2,3,4]))
 [8, 1, 2]
