daf494103c9c93a4abac6e86e6c50d19e79451a5
Yifan Yu <yvifan@gmail.com>
2016-11-28 23:42:11 +0200

    How RecordWildCards bring fields to scope from 'where' clauses as well

---
+++ Test.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE RecordWildCards #-}
+
+data MachCode = MachCode
+  { fstInt    :: Int
+  , sndString :: String
+  }
+
+nullCode = MachCode { fstInt = 0, sndString = "hello" }
+
+cpu :: Int -> (Int, String)
+cpu n = (fstInt, sndString)
+  where
+  MachCode {..} = case n of
+    0 -> nullCode { fstInt = 1 }
+    _ -> nullCode { sndString = "world" }
+
+cpu2 :: Int -> (Int, String)
+cpu2 n = pair
+  where
+  pair = (fstInt, sndString)
+  MachCode {..} = case n of
+    0 -> nullCode { fstInt = 1 }
+    _ -> nullCode { sndString = "world" }
