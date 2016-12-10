2d7497e828a42c162924dea074c7b112b61a835e
Yifan Yu <yvifan@gmail.com>
2016-12-11 00:54:50 +0200

    Finish SSeg.SSeg

--- SSeg/HexToSSeg.hs
+++ SSeg/HexToSSeg.hs
@@ -2,22 +2,29 @@ module SSeg.HexToSSeg where
 
 import CLaSH.Prelude
 
-hexToSSeg :: Unsigned 4 -> Bit -> BitVector 8
-hexToSSeg hex dp =
+-- common anode
+hexToSSegCA :: Bit -> Unsigned 4 -> BitVector 8
+hexToSSegCA dp hex =
   case hex of
-    0x0 -> dp ++# 0b0000001
-    0x1 -> dp ++# 0b1001111
-    0x2 -> dp ++# 0b0010010
-    0x3 -> dp ++# 0b0000110
-    0x4 -> dp ++# 0b1001100
-    0x5 -> dp ++# 0b0100100
-    0x6 -> dp ++# 0b0100000
-    0x7 -> dp ++# 0b0001111
-    0x8 -> dp ++# 0b0000000
-    0x9 -> dp ++# 0b0000100
-    0xA -> dp ++# 0b0001000
-    0xB -> dp ++# 0b1100000
-    0xC -> dp ++# 0b0110001
-    0xD -> dp ++# 0b1000010
-    0xE -> dp ++# 0b0110000
-    _   -> dp ++# 0b0111000
+    0x0 -> dp' ++# 0b1000000
+    0x1 -> dp' ++# 0b1111001
+    0x2 -> dp' ++# 0b0100100
+    0x3 -> dp' ++# 0b0110000
+    0x4 -> dp' ++# 0b0011001
+    0x5 -> dp' ++# 0b0010010
+    0x6 -> dp' ++# 0b0000010
+    0x7 -> dp' ++# 0b1111000
+    0x8 -> dp' ++# 0b0000000
+    0x9 -> dp' ++# 0b0010000
+    0xA -> dp' ++# 0b0001000
+    0xB -> dp' ++# 0b0000011
+    0xC -> dp' ++# 0b1000110
+    0xD -> dp' ++# 0b0100001
+    0xE -> dp' ++# 0b0000110
+    _   -> dp' ++# 0b0001110
+  where
+  dp' = complement dp
+
+-- common cathode
+hexToSSegCC :: Bit -> Unsigned 4 -> BitVector 8
+hexToSSegCC dp hex = complement $ hexToSSegCA dp hex
---
+++ SSeg/SSeg.hs
@@ -0,0 +1,55 @@
+module SSeg.SSeg where
+
+import CLaSH.Prelude
+import SSeg.HexToSSeg
+
+type Hex     = Unsigned 4
+type Counter = Unsigned 20
+
+hexMux :: Counter ->
+          (Hex, Hex, Hex, Hex, Hex, Hex, Hex, Hex) ->
+          (Unsigned 8, Hex)
+hexMux counter (hex7, hex6, hex5, hex4, hex3, hex2, hex1, hex0) =
+  case counter `shiftR` 17 of -- TODO: a more direct way to access the bits?
+    0b000 -> (0b11111110, hex0)
+    0b001 -> (0b11111101, hex1)
+    0b010 -> (0b11111011, hex2)
+    0b011 -> (0b11110111, hex3)
+    0b100 -> (0b11101111, hex4)
+    0b101 -> (0b11011111, hex5)
+    0b110 -> (0b10111111, hex6)
+    _     -> (0b01111111, hex7)
+
+-- dp is ignored
+ssegH :: Signal (Hex, Hex, Hex, Hex, Hex, Hex, Hex, Hex) ->
+         Signal (Unsigned 8, BitVector 8)
+ssegH h = (\(an, hex) -> (an, hexToSSegCA 0 hex)) <$> an_hex
+  where
+  an_hex = hexMux <$> counter <*> h
+
+  counter :: Signal Counter
+  counter = register 0 (counter + 1)
+
+-- from Data.BitVector
+(@@) :: (Integral a, Integral b, Bits a) => a -> (Int, Int) -> b
+a @@ (j,i) = fromIntegral $ (a `shiftR` i) `mod` 2^m
+  where
+  m  = j - i + 1 -- width
+
+ssegU :: Signal (Unsigned 32) -> (Signal (Unsigned 8, BitVector 8))
+ssegU n = ssegH (split <$> n)
+  where
+  split x = let f = (x @@)
+            in (f  (31, 28), f  (27, 24), f  (23, 20), f  (19, 16),
+                f  (15, 12), f  (11, 8),  f  (7,  4),  f  (3,  0))
+
+{-# ANN topEntity
+  (defTop
+    { t_name     = "sseg"
+    , t_inputs   = []
+    , t_outputs  = ["an", "seg"]
+    , t_extraIn  = [("clk", 1), ("btnCpuReset", 1)]
+    , t_extraOut = []
+    , t_clocks   = []
+    }) #-}
+topEntity = ssegU
