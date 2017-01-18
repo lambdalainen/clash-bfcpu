195b050db0a71056e162dd25b5e6d74cba661b3d
Yifan Yu <yvifan@gmail.com>
2017-01-18 15:35:04 +0200

    Add FIFO to uartRx, tested with echo

--- UART/UART.hs
+++ UART/UART.hs
@@ -2,31 +2,35 @@ module UART.UART (uartRx, uartTx, topEntity) where
 
 import CLaSH.Prelude
 import Data.Char
+import Types
 import UART.Baud
+import UART.FIFO
 import UART.Rx
 import UART.Tx
 
-uartRx :: Signal Bit -> (Signal (Unsigned 8), Signal Bool)
-uartRx rx = (rx_dout, rx_done_tick)
+uartRx :: Signal Bit -> Signal Bool -> (Signal Data, Signal Bool)
+uartRx rx rx_read = (fifo_r_data, fifo_empty)
   where
   (rx_dout, rx_done_tick) = mealyB rxRun rxInit (rx, baud_tick)
 
-uartTx :: Signal Bool -> Signal (Unsigned 8) -> (Signal Bit, Signal Bool)
-uartTx tx_start tx_din = (tx, tx_done_tick)
+  (fifo_empty, _, fifo_r_data) = fifo rx_read rx_done_tick rx_dout
+
+uartTx :: Signal Bool -> Signal Data -> (Signal Bit, Signal Bool)
+uartTx tx_write tx_data = (tx, tx_done_tick)
   where
-  (tx, tx_done_tick) = mealyB txRun txInit (tx_start, tx_din, baud_tick)
+  (tx, tx_done_tick) = mealyB txRun txInit (tx_write, tx_data, baud_tick)
 
 {-# ANN topEntity
   (defTop
     { t_name     = "uart"
-    , t_inputs   = ["RsRx"]
+    , t_inputs   = ["RsRx", "btnC"]
     , t_outputs  = ["RsTx"]
     , t_extraIn  = [("clk", 1)]
     , t_extraOut = []
     , t_clocks   = []
     }) #-}
-topEntity :: Signal Bit -> Signal Bit
-topEntity rx = tx
+topEntity :: Signal Bit -> Signal Bool -> Signal Bit
+topEntity rx rx_read = tx
   where
-  (rx_dout, rx_done_tick) = uartRx rx
-  (tx, tx_done_tick)      = uartTx rx_done_tick rx_dout
+  (rx_data, rx_empty) = uartRx rx rx_read
+  (tx, tx_done_tick)  = uartTx rx_read rx_data
