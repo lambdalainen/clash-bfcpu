d9c985e3a5ebb1aecd704d3c4d4b04cd5637b29f
Yifan Yu <yvifan@gmail.com>
2017-01-19 00:55:20 +0200

    `view` doesn't give the updated value, only `use` does

---
+++ Mealy.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Mealy where
+
+import CLaSH.Prelude
+import Control.Lens
+import Control.Monad
+import Control.Monad.Trans.State
+import Data.Tuple
+import Types
+
+data MealyState = MealyState { _mealy_data  :: Data }
+
+makeLenses ''MealyState
+
+mealyInit :: MealyState
+mealyInit = MealyState 0
+
+mealyRun :: MealyState -> Bool -> (MealyState, (Data, Data))
+mealyRun s@(MealyState {..}) test_view = swap $ flip runState s $ do
+  mealy_data += 1
+  let even     = _mealy_data `mod` 2 == 0
+      view_val = view mealy_data s
+  use_val <- use mealy_data
+  return (_mealy_data, if test_view then view_val else use_val)
+
+mealyTest :: Signal Bool -> (Signal Data, Signal Data)
+mealyTest test_view = mealyB mealyRun mealyInit test_view
